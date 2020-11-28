9c0340b3d4963a39e5810e2d15f73902593405ef
David Rickett <info@bowenliquorstore.ca>
2020-11-27 16:32:31 -0800

    forgot to commit this

--- emailwatcher/sync-mail.py
+++ emailwatcher/sync-mail.py
@@ -5,7 +5,7 @@ import time
 import schedule
 import json
 
-from mailattachmentsarchiver import mailattachmentsarchiver as maa
+from mailattachmentsarchiver import mailattachmentsarchiver as maa_app
 
 def generateIMAP(file,addr,port,user,pwd):
     credentials = {}
@@ -13,25 +13,32 @@ def generateIMAP(file,addr,port,user,pwd):
     credentials['user']=user
     credentials['password']=pwd
 
+    print('connecting to server %s with username %s'%(addr,user))
+
     with open(file, 'w') as fp:
 	    json.dump(credentials,fp,indent=2,separators=(',', ': '),sort_keys=True)
 
 def getmail(maa):
-    print("Getting mail")
+    print("Checking Mail")
     maa.get_mail()
 
-def main(maa):
-    #every day at noon, check for mail.
-    schedule.every(4).hours.do(getmail,maa)
+def main(maa, hours):
+    #if we don't define it, its an hour interval
+    if hours is None or hours == '' or not hours.isDigit(): hours = 1
 
-    print("Mail scheduler started")
+    print('Script started, initial mail check commencing')
+    maa.get_mail()
+
+    #every day at noon, check for mail.
+    schedule.every(int(hours)).hours.do(getmail,maa)
+    print(f'Will check mail every {hours} hour(s) from now on.')
     while True:
         schedule.run_pending()
         time.sleep(60)
 
 if __name__ == '__main__':
     #generate IMAP from docker .env
-    generateIMAP('/tmp/imap.json',os.getenv('IMAP_ADDR'),os.getenv('IMAP_USER'),os.getenv('IMAP_PASS'))
+    generateIMAP('/tmp/imap.json',os.getenv('IMAP_ADDR'),os.getenv('IMAP_PORT'),os.getenv('IMAP_USER'),os.getenv('IMAP_PASS'))
 
-    maa = maa('/tmp/imap.json','/usr/share/config.json')
-    main(maa)
+    maa = maa_app('/tmp/imap.json','/usr/share/config.json')
+    main(maa, os.getenv('SYNCTIME'))
